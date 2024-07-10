605907f4ac2dc1d062b8c4103f086c481f2edba9
Terer <terermike3@gmail.com>
2024-07-11 02:24:22 +0300

    Used the useRoteError hook

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,3 +1,27 @@
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+
 export default function ErrrorPage() {
-  return <p>An error occurred!!</p>;
+  const error = useRouteError();
+
+  let title = "An error occurred";
+  let message = "Something went wrong";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find page";
+  }
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
@@ -12,7 +12,7 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //...
+    throw json({ message: "Could not fetch events" }, { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
